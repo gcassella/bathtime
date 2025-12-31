5e8622cb3744d7e1adadd0defe031091beeb1c93
gwmcassella <gwmcassella@gmail.com>
2025-12-31 10:47:33 +0000

    Reorganize file tree

--- BathTime/Stinkiness/EntityBehaviorStinky.cs
+++ BathTime/Stinkiness/EntityBehaviorStinky.cs
@@ -1,8 +1,8 @@
 using System;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
-using Vintagestory.API.MathTools;
 
 namespace BathTime;
 
